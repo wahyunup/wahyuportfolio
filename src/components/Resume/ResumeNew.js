21e32bd11cdf0d1887e7d969d5390ca9c83247e0
wahyunup <wahyunurpambuko@gmail.com>
2024-12-17 08:08:32 +0700

    update:CV

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/CV_WAHYU NUR PAMBUKO_ FRONT END DEVELOPER - Copy.pdf";
+import pdf from "../../Assets/../Assets/CV_WAHYU NUR PAMBUKO_ FRONT END DEVELOPER.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
